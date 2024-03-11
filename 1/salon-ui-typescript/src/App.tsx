c72e7f4717484da15b251dca29b0f897e36936a2
Tobias Hochguertel <tobias.hochguertel@googlemail.com>
2024-03-11 20:15:41 +0100

    WiP

--- 1/salon-ui-typescript/src/App.tsx
+++ 1/salon-ui-typescript/src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Header.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from "react-bootstrap";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <Container>
+                <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                    <a href="/"
+                       className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                        {/*                        <svg className="bi me-2" width="40" height="32">
+                            <use href="#"></use>
+                        </svg>*/}
+                        <span className="fs-4">Ar Salon & Day Spa</span>
+                    </a>
+
+                    {/*                    <ul className="nav nav-pills">
+                        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a>
+                        </li>
+                        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+                    </ul>*/}
+                </header>
+            </Container>
+            <Container>
+                Edit <span style={{color: "red"}}>`src/App.js`</span> and save to reload.
+            </Container>
+        </>
     );
 }
 
