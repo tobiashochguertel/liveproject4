59cb524e8948f2409dd226ce48c1a5091a0ff92d
Tobias Hochguertel <tobias.hochguertel@googlemail.com>
2024-03-12 11:45:25 +0100

    WiP

--- 1/salon-api/src/main/java/work/hochguertel/salonapi/SalonApiApplication.java
+++ 1/salon-api/src/main/java/work/hochguertel/salonapi/SalonApiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SalonDetails.class)
@@ -26,6 +29,17 @@ public class SalonApiApplication {
         var save = salonRepository.save(salon);
     }
 
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**").allowedOrigins("*");
+            }
+
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SalonApiApplication.class, args);
